1b340095b8b7950a2ca42d6595a9fc73ae0acd86
YCooE <31769542+YCooE@users.noreply.github.com>
2019-06-13 11:03:31 +0200

    Added a bunch of stuff
    Signed-off-by: YCooE <31769542+YCooE@users.noreply.github.com>

---
+++ 12Daysleft/Assets/Scripts/Distancegrab.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class Distancegrab : MonoBehaviour
+{
+    SteamVR_TrackedObject trackedObject;
+    SteamVR_Controller.Device device; 
+    // Awake is called when program is called
+    void Awake()
+    {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // Fixedupdate is every frame 90 frames per second
+    void FixedUpdate()
+    {
+        device = SteamVR_Controller.Input((int)trackedObject.index);
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("Holding down the trigger");
+        }
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("Touchdown the trigger");
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        Debug.Log("Trigger stay");
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("Collision");
+            col.gameObject.transform.SetParent(gameObject.transform);
+            col.attachedRigidbody.isKinematic = true;
+        }
+
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            col.gameObject.transform.SetParent(null);
+            col.attachedRigidbody.isKinematic = false;
+
+            tossObject(col.attachedRigidbody);
+        }
+    }
+    
+    void tossObject(Rigidbody rigid)
+    {
+        // if origin exist nice, if not we take the parent
+        Transform origin = trackedObject.origin ? trackedObject.origin : trackedObject.transform.parent;
+        if (origin != null)
+        {
+            rigid.velocity = origin.TransformVector(device.velocity);
+            rigid.angularVelocity = origin.TransformVector(device.angularVelocity);
+        }
+        else
+        {
+            rigid.velocity = device.velocity;
+            rigid.angularVelocity = device.angularVelocity;
+        }
+    }
+}
